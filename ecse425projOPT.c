a71453ab1a76efc84845d7f6e9be820577e87335
Ryan Singzon <rcsingzon@gmail.com>
2014-04-02 02:11:24 -0400

    Trying out loop tiling for optimized matrix-matrix

--- ecse425projOPT.c
+++ ecse425projOPT.c
@@ -10,12 +10,57 @@ void matVecMult_opt(int N, const double *matA, const double *vecB, double *vecC)
 
 void matMult_opt(int N, const double *matA, const double *matB, double *matC) 
 {
-	int i, j, k;
+	int i, j, k, jj, kk;
+	int reuseFactor = 2;
 	int aIndex, bIndex, cIndex;
+	double aValue;
 
-    //Iterate through rows of A,C, columns of B
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
+
+    // Separate the matrix into smaller chunks
+    for (kk = 0; kk < N; kk = kk + reuseFactor) {
+        for (jj = 0; jj < N; jj = jj + reuseFactor) {
+        	for (i = 0; i < N; i++){
+
+        		int kMin;
+        		if ( (kk + reuseFactor - 1) < N ){
+        			kMin = kk + reuseFactor - 1;
+        		}
+        		else{
+        			kMin = N;
+        		}
+
+        		for (k = kk; k <= kMin; k++){
+        			aIndex = i*N + k;
+        			aValue = matA[aIndex];
+        			
+
+        			int jMin;
+        			if ( (jj + reuseFactor - 1) < N){
+        				jMin = jj + reuseFactor -1;
+        			}
+        			else{
+        				jMin = N;
+        			}
+
+        			for(j = jj; j < jMin; j++){
+        				bIndex = k*N + j;
+        				cIndex = i*N + j;
+
+        			//	printf("Index: %d\n", bIndex);
+
+        				matC[cIndex] += aValue * matB[bIndex];
+        			}
+        		}
+        	}
+        //	printf("break\n");
+        }
+    }
+        	
+/*
+	for(i = 0; i < N; i++){
+		for(j = 0; j < N; j++){
+			
+
 
         	// Sum total
         	double sumTotal = 0.0;    
@@ -31,4 +76,5 @@ void matMult_opt(int N, const double *matA, const double *matB, double *matC)
             matC[cIndex] = sumTotal;
         }
     }
+    */
 }
