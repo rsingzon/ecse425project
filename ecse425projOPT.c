3fba21367260ca8869908aa0191122da8a2683f0
Ryan Singzon <rcsingzon@gmail.com>
2014-04-01 23:40:32 -0400

    Added naive implementation of matrix multiplication. To be optimized

--- ecse425projOPT.c
+++ ecse425projOPT.c
@@ -1,14 +1,34 @@
 #include "ecse425projOPT.h"
 #include "ecse425proj.h"
 
+#include <stdio.h>
 
 void matVecMult_opt(int N, const double *matA, const double *vecB, double *vecC) 
 {
-    // Code in your optimized implementation here
+    
 }
 
 void matMult_opt(int N, const double *matA, const double *matB, double *matC) 
 {
-    // Code in your optimized implementation here
-}
+	int i, j, k;
+	int aIndex, bIndex, cIndex;
 
+    //Iterate through rows of A,C, columns of B
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+
+        	// Sum total
+        	double sumTotal = 0.0;    
+
+        	for (k = 0; k < N; k++){
+        		aIndex = i*N + k;
+        		bIndex = k*N + j;
+
+        		sumTotal = sumTotal + matA[aIndex] * matB[bIndex];
+        	}
+               	
+        	cIndex = i*N + j;
+            matC[cIndex] = sumTotal;
+        }
+    }
+}
