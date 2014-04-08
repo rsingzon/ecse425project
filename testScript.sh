ad3b13e799027d86c508b2706212fd95dd3ec765
Ryan Singzon <rcsingzon@gmail.com>
2014-04-08 00:46:26 -0400

    Added script to run program with various inputs

---
+++ testScript.sh
@@ -0,0 +1,14 @@
+build/./ecse425proj -N 10 -operation 0
+build/./ecse425proj -N 100 -operation 0
+build/./ecse425proj -N 200 -operation 0
+build/./ecse425proj -N 300 -operation 0
+build/./ecse425proj -N 400 -operation 0
+build/./ecse425proj -N 500 -operation 0
+build/./ecse425proj -N 600 -operation 0
+build/./ecse425proj -N 700 -operation 0
+build/./ecse425proj -N 800 -operation 0
+build/./ecse425proj -N 900 -operation 0
+build/./ecse425proj -N 1000 -operation 0
+build/./ecse425proj -N 1500 -operation 0
+build/./ecse425proj -N 2000 -operation 0
+build/./ecse425proj -N 3000 -operation 0
