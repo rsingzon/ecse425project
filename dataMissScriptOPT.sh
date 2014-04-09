3474aa55ed415f068d3162498a74d18a541d192a
Ryan Singzon <rcsingzon@gmail.com>
2014-04-09 00:37:19 -0400

    All, N is 100

--- dataMissScriptOPT.sh
+++ dataMissScriptOPT.sh
@@ -1,46 +1,46 @@
 #Varying L1D block size
 #4MB LL cache
-valgrind --tool=cachegrind --I1=32768,8,512 --D1=4096,8,64   --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive	#8B block
-valgrind --tool=cachegrind --I1=32768,8,512 --D1=4096,8,128   --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive	#16B block
-valgrind --tool=cachegrind --I1=32768,8,512 --D1=4096,8,256   --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive	#32B block
-valgrind --tool=cachegrind --I1=32768,8,512 --D1=4096,8,512   --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-valgrind --tool=cachegrind --I1=32768,8,512 --D1=4096,16,512   --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-valgrind --tool=cachegrind --I1=32768,8,512 --D1=4096,32,512   --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=4096,8,64   	--LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive	#8B block
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=4096,8,128   	--LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive	#16B block
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=4096,8,256   	--LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive	#32B block
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=4096,8,512   	--LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=4096,16,512   	--LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=4096,32,512   	--LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
 
-valgrind --tool=cachegrind --I1=32768,8,512 --D1=8192,8,64   --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-valgrind --tool=cachegrind --I1=32768,8,512 --D1=8192,8,128   --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-valgrind --tool=cachegrind --I1=32768,8,512 --D1=8192,8,256   --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-valgrind --tool=cachegrind --I1=32768,8,512 --D1=8192,8,512   --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-valgrind --tool=cachegrind --I1=32768,8,512 --D1=8192,8,1024   --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-valgrind --tool=cachegrind --I1=32768,8,512 --D1=8192,8,2048   --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=8192,8,64   	--LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=8192,8,128   	--LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=8192,8,256   	--LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=8192,8,512   	--LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=8192,8,1024  	--LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=8192,8,2048  	--LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
 
-#valgrind --tool=cachegrind --I1=32768,8,512 --D1=16384,8,64   --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-#valgrind --tool=cachegrind --I1=32768,8,512 --D1=16384,8,128   --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-#valgrind --tool=cachegrind --I1=32768,8,512 --D1=16384,8,256   --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-#valgrind --tool=cachegrind --I1=32768,8,512 --D1=16384,8,512   --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-#valgrind --tool=cachegrind --I1=32768,8,512 --D1=16384,8,1024  --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-#valgrind --tool=cachegrind --I1=32768,8,512 --D1=16384,8,2048  --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-#
-#valgrind --tool=cachegrind --I1=32768,8,512 --D1=32768,8,64  --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-#valgrind --tool=cachegrind --I1=32768,8,512 --D1=32768,8,128  --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-#valgrind --tool=cachegrind --I1=32768,8,512 --D1=32768,8,256  --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-#valgrind --tool=cachegrind --I1=32768,8,512 --D1=32768,8,512  --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-#valgrind --tool=cachegrind --I1=32768,8,512 --D1=32768,8,1024  --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-#valgrind --tool=cachegrind --I1=32768,8,512 --D1=32768,8,2048 --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=16384,8,64  	--LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=16384,8,128 	--LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=16384,8,256 	--LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=16384,8,512 	--LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=16384,8,1024	--LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=16384,8,2048	--LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
 
-#valgrind --tool=cachegrind --I1=32768,8,512 --D1=65536,8,64    --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-#valgrind --tool=cachegrind --I1=32768,8,512 --D1=65536,8,128   --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-#valgrind --tool=cachegrind --I1=32768,8,512 --D1=65536,8,256   --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-#valgrind --tool=cachegrind --I1=32768,8,512 --D1=65536,8,512   --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-#valgrind --tool=cachegrind --I1=32768,8,512 --D1=65536,8,1024  --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-#valgrind --tool=cachegrind --I1=32768,8,512 --D1=65536,8,2048  --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-#
-#valgrind --tool=cachegrind --I1=32768,8,512 --D1=131072,8,64   --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-#valgrind --tool=cachegrind --I1=32768,8,512 --D1=131072,8,128  --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-#valgrind --tool=cachegrind --I1=32768,8,512 --D1=131072,8,256  --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-#valgrind --tool=cachegrind --I1=32768,8,512 --D1=131072,8,512  --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-#valgrind --tool=cachegrind --I1=32768,8,512 --D1=131072,8,1024 --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
-#valgrind --tool=cachegrind --I1=32768,8,512 --D1=131072,8,2048 --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=32768,8,64  	--LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=32768,8,128 	--LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=32768,8,256 	--LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=32768,8,512 	--LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=32768,8,1024	--LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=32768,8,2048  --LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=65536,8,64    --LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=65536,8,128   --LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=65536,8,256   --LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=65536,8,512   --LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=65536,8,1024  --LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=65536,8,2048  --LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=131072,8,64   --LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=131072,8,128  --LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=131072,8,256  --LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=131072,8,512  --LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=131072,8,1024 --LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=131072,8,2048 --LL=4194304,16,1024  build/./ecse425proj -N 100 -operation 0 -naive
 
 
 #8MB LL cache
