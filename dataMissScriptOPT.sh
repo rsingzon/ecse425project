962518d925efbdbd130aad2c66a8786e50e41581
Ryan Singzon <rcsingzon@gmail.com>
2014-04-08 23:48:25 -0400

    Changed cache size and associativitiy to powers of 2

--- dataMissScriptOPT.sh
+++ dataMissScriptOPT.sh
@@ -1,14 +1,23 @@
 #Varying L1D cache size
-valgrind --tool=cachegrind --I1=32768,8,512 --D1=1024,8,512 --LL=6291456,24,1536  build/./ecse425proj -N 1000 -operation 0 -OPT
-valgrind --tool=cachegrind --I1=32768,8,512 --D1=2048,8,512 --LL=6291456,24,1536  build/./ecse425proj -N 1000 -operation 0 -OPT
-valgrind --tool=cachegrind --I1=32768,8,512 --D1=4096,8,512 --LL=6291456,24,1536  build/./ecse425proj -N 1000 -operation 0 -OPT
-valgrind --tool=cachegrind --I1=32768,8,512 --D1=8192,8,512 --LL=6291456,24,1536  build/./ecse425proj -N 1000 -operation 0 -OPT
-valgrind --tool=cachegrind --I1=32768,8,512 --D1=16384,8,512 --LL=6291456,24,1536  build/./ecse425proj -N 1000 -operation 0 -OPT
-valgrind --tool=cachegrind --I1=32768,8,512 --D1=32768,8,512 --LL=6291456,24,1536  build/./ecse425proj -N 1000 -operation 0 -OPT
-valgrind --tool=cachegrind --I1=32768,8,512 --D1=65536,8,512 --LL=6291456,24,1536  build/./ecse425proj -N 1000 -operation 0 -OPT
-valgrind --tool=cachegrind --I1=32768,8,512 --D1=131072,8,512 --LL=6291456,24,1536  build/./ecse425proj -N 1000 -operation 0 -OPT
-valgrind --tool=cachegrind --I1=32768,8,512 --D1=262144,8,512 --LL=6291456,24,1536  build/./ecse425proj -N 1000 -operation 0 -OPT
-valgrind --tool=cachegrind --I1=32768,8,512 --D1=524288,8,512 --LL=6291456,24,1536  build/./ecse425proj -N 1000 -operation 0 -OPT
+#4MB LL cache
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=4096,8,512   --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -OPT
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=8192,8,512   --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -OPT
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=16384,8,512  --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -OPT
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=32768,8,512  --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -OPT
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=65536,8,512  --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -OPT
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=131072,8,512 --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -OPT
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=262144,8,512 --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -OPT
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=524288,8,512 --LL=4194304,16,1024  build/./ecse425proj -N 256 -operation 0 -OPT
+
+#8MB LL cache
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=4096,8,512   --LL=8388608,32,2048  build/./ecse425proj -N 256 -operation 0 -OPT
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=8192,8,512   --LL=8388608,32,2048  build/./ecse425proj -N 256 -operation 0 -OPT
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=16384,8,512  --LL=8388608,32,2048  build/./ecse425proj -N 256 -operation 0 -OPT
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=32768,8,512  --LL=8388608,32,2048  build/./ecse425proj -N 256 -operation 0 -OPT
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=65536,8,512  --LL=8388608,32,2048  build/./ecse425proj -N 256 -operation 0 -OPT
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=131072,8,512 --LL=8388608,32,2048  build/./ecse425proj -N 256 -operation 0 -OPT
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=262144,8,512 --LL=8388608,32,2048  build/./ecse425proj -N 256 -operation 0 -OPT
+valgrind --tool=cachegrind --I1=32768,8,512 --D1=524288,8,512 --LL=8388608,32,2048  build/./ecse425proj -N 256 -operation 0 -OPT
 
 #I1 cache:         32768 B, 64 B, 8-way associative
 #D1 cache:         32768 B, 64 B, 8-way associative
